b68f9dd3bc9c1fc1da2e58bdd4402b87fe2d6337
Sidraj <siddharth.rajguru2013@gmail.com>
2017-02-16 17:45:06 -0600

    Created Linked List manipulation: - Create List - Return pointer - Print List - Replicate List - Return pointer

---
+++ Datastructures/Datastructures/main.cpp
@@ -0,0 +1,92 @@
+//
+//  main.cpp
+//  Datastructures
+//
+//  Created by Siddharth Rajguru on 2/16/17.
+//  Copyright © 2017 Siddharth Rajguru. All rights reserved.
+//  Link List manipulation
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+struct node* head2 = NULL;   //Global Replica Head
+struct node* head = NULL;   //Global Head
+struct node {
+   int x = 0;
+    struct node* next;
+};
+
+// Return type struct pointer: Insert Node to the list in-line. Returns pointer to new Head.
+
+struct node* insertNode(int val){
+    struct node* headx;
+    struct node* temp;
+    
+    if(headx != NULL){
+        temp = new (struct node);
+        temp->x = val;
+        temp->next = head;
+        headx = temp;
+    }
+    
+    if(headx == NULL){
+        temp = new (struct node);
+        temp->x = val;
+        temp->next = NULL;
+        headx = temp;
+    }
+    return headx;
+    
+};
+
+// Prints list: Takes a pointer argument for Head node.
+
+void printList(struct node* headNode){
+    struct node* temp1;
+   
+    
+    temp1 = headNode;
+    while(temp1->next!=NULL){
+        
+        printf("%d\n", temp1->x);
+        temp1 = temp1->next;
+    }
+    if (temp1->next == NULL){
+        printf("%d\n", temp1->x);
+    }
+   }
+
+//Replicate list: Void function takes
+
+struct node* replicateList(struct node* headnode){
+    
+    struct node* temp2;
+    struct node* newHead = NULL;
+    temp2 = headnode;
+    while(temp2->next!=NULL){
+        newHead  = insertNode(temp2->x);
+        temp2 = temp2->next;
+    }
+    return newHead;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    head = insertNode(8);
+    head = insertNode(7);
+    head = insertNode(6);
+    head = insertNode(5);
+    head = insertNode(4);
+    head = insertNode(3);
+    head = insertNode(2);
+    head2 = replicateList(head);
+    printList(head);
+    printList(head2);
+    
+    return 0;
+}
